653df2154574bb339daf82921618779afdf87fae
beksiazek <beksiazek@gmail.com>
2023-03-28 17:09:10 -0300

    Header, Nav and About components

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,53 @@
-import React from 'react';
+import React from "react";
 import "./about.css";
+import ME from "../../assets/about-me.png";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 export default function About() {
-  return (
-    <div>About</div>
-  )
+	return (
+		<section id="about">
+			<h5>Get to know</h5>
+			<h2>ABout Me</h2>
+			<div className="container about__container">
+				<div className="about__me">
+					<div className="about__me-image">
+						<div className="img-mask">
+							<img src={ME} alt="About image" />
+						</div>
+					</div>
+				</div>
+				<div className="about__content">
+					<div className="about__cards">
+						<article className="about__card">
+							<FaAward className="about__icon" />
+							<h5>Experience</h5>
+							<small>1+ Year working</small>
+						</article>
+						<article className="about__card">
+							<FiUsers className="about__icon" />
+							<h5>Clients</h5>
+							<small>A lot of clients worldwide</small>
+						</article>
+						<article className="about__card">
+							<VscFolderLibrary className="about__icon" />
+							<h5>Projects</h5>
+							<small>3 Completed</small>
+						</article>
+					</div>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Ipsa itaque doloremque suscipit sint neque cupiditate
+						quisquam consequatur, voluptatem expedita assumenda,
+						molestiae, vitae dignissimos repellat aspernatur
+						adipisci cumque a aliquid nemo!
+					</p>
+					<a href="#contact" className="btn btn-primary">
+						Let's Talk
+					</a>
+				</div>
+			</div>
+		</section>
+	);
 }
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,49 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
 import "./nav.css";
+import { AiOutlineUser } from "react-icons/ai";
+import { BiHomeAlt2, BiBook, BiMessageSquareDetail } from "react-icons/bi";
+import { RiServiceLine } from "react-icons/ri";
 
 export default function Nav() {
-  return (
-    <div>Nav</div>
-  )
+	const [activeNav, setActiveNav] = useState("#");
+	return (
+		<nav>
+			<a
+				href="#"
+				onClick={() => setActiveNav("#")}
+				className={activeNav === "#" ? "active" : ""}
+			>
+				<BiHomeAlt2 />
+			</a>
+			<a
+				href="#about"
+				onClick={() => setActiveNav("#about")}
+				className={activeNav === "#about" ? "active" : ""}
+			>
+				<AiOutlineUser />
+			</a>
+			<a
+				href="#experience"
+				onClick={() => setActiveNav("#experience")}
+				className={activeNav === "#experience" ? "active" : ""}
+			>
+				<BiBook />
+			</a>
+			<a
+				href="#services"
+				onClick={() => setActiveNav("#services")}
+				className={activeNav === "#services" ? "active" : ""}
+			>
+				<RiServiceLine />
+			</a>
+			<a
+				href="#contact"
+				onClick={() => setActiveNav("#contact")}
+				className={activeNav === "#contact" ? "active" : ""}
+			>
+				<BiMessageSquareDetail />
+			</a>
+		</nav>
+	);
 }
