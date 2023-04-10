cbc94f9553258e8c25a0a1dd35505d43567f8d17
beksiazek <beksiazek@gmail.com>
2023-04-10 04:53:33 -0300

    + CV download dropdown menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import text_en from "./text/text_en.json";
 import text_es from "./text/text_es.json";
-import CV_en from "./assets/cv/CV-BrianKsiazek(EN).pdf";
-import CV_es from "./assets/cv/CV-BrianKsiazek(ES).pdf";
+import cv_en from "./assets/cv/CV-BrianKsiazek(EN).pdf";
+import cv_es from "./assets/cv/CV-BrianKsiazek(ES).pdf";
 import LangSelector from "./components/langSelector/LangSelector";
 import Nav from "./components/nav/Nav";
 import Header from "./components/header/Header";
@@ -16,12 +16,12 @@ import Footer from "./components/footer/Footer";
 export default function App() {
 	const [language, setLanguage] = useState(navigator.language.slice(0, 2));
 	const text = language === "en" ? text_en : language === "es" ? text_es : {};
-	const cv = language === "en" ? CV_en : language === "es" ? CV_es : "";
+
 	return (
 		<>
 			<LangSelector language={language} setLanguage={setLanguage} />
 			<Nav text={text.nav} />
-			<Header cv={cv} text={text.header} />
+			<Header cv_en={cv_en} cv_es={cv_es} text={text.header} />
 			<About text={text.about} />
 			<Experience text={text.experience} />
 			<Services text={text.services} />
--- src/components/header/Cta.jsx
+++ src/components/header/Cta.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
+import { HiDownload } from "react-icons/hi";
 
 export default function Cta(props) {
-  const { text, cv } = props;
-  return (
-    <div className='cta'>
-        <a href={cv} download className='btn'>{text.cv}</a>
-        <a href='#contact' className='btn btn-primary'>{text.talk}</a>
-    </div>
-  )
+	const { text, cv_en, cv_es } = props;
+	return (
+		<div className="cta">
+			<div className="dropdown">
+      <a className="btn">
+					{text.cv}
+				</a>
+				<div className="dropdown-content dropdown-content--animated dropdown-content-6">
+					<a className="cv-download" href={cv_en} download>{text.en} <HiDownload /></a>
+					<a className="cv-download" href={cv_es} download>{text.es} <HiDownload /></a>
+				</div>
+			</div>
+			<a href="#contact" className="btn btn-primary">
+				{text.talk}
+			</a>
+		</div>
+	);
 }
